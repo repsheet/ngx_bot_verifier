261d63e075d00f458524e4eb11b2723e23a90bf5
Aaron Bedra <aaron@aaronbedra.com>
2017-07-09 15:29:20 -0500

    Accept X-Forwarded-For header for connected IP

--- ngx_http_bot_verifier_module.c
+++ ngx_http_bot_verifier_module.c
@@ -10,18 +10,95 @@ typedef struct {
 } ngx_http_bot_verifier_module_loc_conf_t;
 
 static ngx_int_t
+remote_address(char *connected_address, char *xff_header, char *address)
+{
+  if ((connected_address == NULL && xff_header == NULL) || address == NULL) {
+    return NGX_DECLINED;
+  }
+
+  int length;
+  memset(address, '\0', INET_ADDRSTRLEN);
+
+  if (xff_header != NULL) {
+    char *p;
+
+    for (p = xff_header; p < (xff_header + strlen(xff_header)); p++) {
+      if (*p == ' ' || *p == ',') {
+        break;
+      }
+    }
+
+    length = p - xff_header;
+    char test_address[length + 1];
+    memcpy(test_address, xff_header, length);
+    test_address[length] = '\0';
+
+    unsigned char buf[sizeof(struct in_addr)];
+
+    if (inet_pton(AF_INET, (const char *)test_address, buf) == 1) {
+      memcpy(address, test_address, length);
+      return NGX_OK;
+    } else {
+      return NGX_ERROR;
+    }
+  } else {
+    memcpy(address, connected_address, strlen(connected_address));
+    return NGX_OK;
+  }
+}
+
+static ngx_int_t
+ngx_http_bot_verifier_module_determine_address(ngx_http_request_t *r, char *address)
+{
+  ngx_int_t result;
+  ngx_table_elt_t *xff = NULL;
+  ngx_array_t *ngx_array = &r->headers_in.x_forwarded_for;
+  if (ngx_array != NULL && ngx_array->nelts > 0) {
+    ngx_table_elt_t **first_elt = ngx_array->elts;
+    xff = first_elt[0];
+  }
+
+  if (xff == NULL) {
+    memcpy(address, r->connection->addr_text.data, r->connection->addr_text.len);
+    address[r->connection->addr_text.len] = '\0';
+    return NGX_OK;
+  } else {
+    result = remote_address((char *)r->connection->addr_text.data, (char*)xff->value.data, address);
+    if (result == NGX_OK) {
+      return NGX_OK;
+    } else if (result == NGX_DECLINED) {
+      ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Not enough information to determine connecting IP address");
+      return NGX_ERROR;
+    } else {
+      ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "The address supplied is not a valid IP address");
+      return NGX_ERROR;
+    }
+  }
+}
+
+static ngx_int_t
 ngx_http_bot_verifier_module_verify_bot(ngx_http_request_t *r)
 {
+  char dervied_address[INET_ADDRSTRLEN];
+  ngx_int_t error = ngx_http_bot_verifier_module_determine_address(r, dervied_address);
+  if (error == NGX_ERROR || error == NGX_DECLINED) {
+    return NGX_ERROR;
+  }
+
+  ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Using %s as connected address", &dervied_address);
+
   struct sockaddr_in sa;
   sa.sin_family = AF_INET;
-  inet_pton(AF_INET, (char *)r->connection->addr_text.data, &(sa.sin_addr));
+  inet_pton(AF_INET, dervied_address, &(sa.sin_addr));
   char hostname[NI_MAXHOST];
 
-  int error = getnameinfo((struct sockaddr *) &sa, sizeof(sa), hostname, sizeof(hostname), NULL, 0, 0);
+  error = getnameinfo((struct sockaddr *) &sa, sizeof(sa), hostname, sizeof(hostname), NULL, 0, 0);
   if (error != 0) {
     ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "getnameinfo() error: %s", gai_strerror(error));
     return NGX_ERROR;
   }
+
+  ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Lookup hostname %s", &hostname);
 
   struct addrinfo *result;
   error = getaddrinfo(hostname, NULL, NULL, &result);
@@ -33,7 +110,9 @@ ngx_http_bot_verifier_module_verify_bot(ngx_http_request_t *r)
   struct sockaddr_in *forward = (struct sockaddr_in*)result->ai_addr;
   char *forward_result = inet_ntoa(forward->sin_addr);
 
-  if (strcmp(hostname, forward_result) == 0) {
+  ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Lookup IP %s", forward_result);
+
+  if (strcmp(dervied_address, forward_result) == 0) {
     freeaddrinfo(result);
     return NGX_OK;
   } else {
@@ -75,16 +154,11 @@ ngx_http_bot_verifier_module_identifies_as_known_bot(ngx_http_request_t *r)
 
   if (n >= 0) {
     ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Google bot identified for %V", &user_agent);
-    ngx_int_t ret = ngx_http_bot_verifier_module_verify_bot(r);
-    if (ret == NGX_OK) {
-      ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Verification successful");
-    } else if (ret == NGX_DECLINED) {
-      ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Verification failed");
-      return NGX_HTTP_FORBIDDEN;
-    }
+    return NGX_OK;
   }
 
   ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "User Agent %V not identified", &user_agent);
+
   return NGX_DECLINED;
 }
 
@@ -102,10 +176,16 @@ ngx_http_bot_verifier_module_handler(ngx_http_request_t *r)
   }
 
   ngx_int_t ret = ngx_http_bot_verifier_module_identifies_as_known_bot(r);
+
   if (ret == NGX_OK) {
     ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Bot identity detected");
-  } else if (ret == NGX_HTTP_FORBIDDEN) {
-    return NGX_HTTP_FORBIDDEN;
+    ret = ngx_http_bot_verifier_module_verify_bot(r);
+    if (ret == NGX_OK) {
+      ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Verification successful");
+    } else if (ret == NGX_DECLINED) {
+      ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Verification failed");
+      return NGX_HTTP_FORBIDDEN;
+    }
   } else {
     ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Bot does not identify");
   }
