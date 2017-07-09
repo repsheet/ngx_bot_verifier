d84cfe060470509c5673d653a4ddce10c77d0156
Aaron Bedra <aaron@aaronbedra.com>
2017-07-08 19:10:57 -0500

    Adding directive for enable/disable

--- ngx_http_bot_verifier_module.c
+++ ngx_http_bot_verifier_module.c
@@ -5,10 +5,20 @@
 
 ngx_module_t ngx_http_bot_verifier_module;
 
+typedef struct {
+  ngx_flag_t enabled;
+} ngx_http_bot_verifier_module_loc_conf_t;
+
 static ngx_int_t
 ngx_http_bot_verifier_module_handler(ngx_http_request_t *r)
 {
   if (r->main->internal) {
+    return NGX_DECLINED;
+  }
+
+  ngx_http_bot_verifier_module_loc_conf_t *loc_conf = ngx_http_get_module_loc_conf(r, ngx_http_bot_verifier_module);
+
+  if (!loc_conf->enabled || loc_conf->enabled == NGX_CONF_UNSET) {
     return NGX_DECLINED;
   }
 
@@ -35,28 +45,67 @@ ngx_http_bot_verifier_module_init(ngx_conf_t *cf)
   return NGX_OK;
 }
 
+static ngx_command_t
+ngx_http_bot_verifier_module_commands[] = {
+  {
+    ngx_string("bot_verifier"),
+    NGX_HTTP_MAIN_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_flag_slot,
+    NGX_HTTP_LOC_CONF_OFFSET,
+    offsetof(ngx_http_bot_verifier_module_loc_conf_t, enabled),
+    NULL
+  },
+  ngx_null_command
+};
+
+static void*
+ngx_http_bot_verifier_module_create_loc_conf(ngx_conf_t *cf)
+{
+  ngx_http_bot_verifier_module_loc_conf_t *conf;
+
+  conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_bot_verifier_module_loc_conf_t));
+  if (conf == NULL) {
+    return NULL;
+  }
+
+  conf->enabled = NGX_CONF_UNSET;
+
+  return conf;
+}
+
+static char*
+ngx_http_bot_verifier_module_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+  ngx_http_bot_verifier_module_loc_conf_t *prev = (ngx_http_bot_verifier_module_loc_conf_t *) parent;
+  ngx_http_bot_verifier_module_loc_conf_t *conf = (ngx_http_bot_verifier_module_loc_conf_t *) child;
+
+  ngx_conf_merge_value(conf->enabled, prev->enabled, 0);
+
+  return NGX_CONF_OK;
+}
+
 static ngx_http_module_t ngx_http_bot_verifier_module_ctx = {
-  NULL,                               /* preconfiguration */
-  ngx_http_bot_verifier_module_init,  /* postconfiguration */
-  NULL,                               /* create main configuration */
-  NULL,                               /* init main configuration */
-  NULL,                               /* create server configuration */
-  NULL,                               /* merge server configuration */
-  NULL,                               /* create location configuration */
-  NULL                                /* merge location configuration */
+  NULL,                                         /* preconfiguration */
+  ngx_http_bot_verifier_module_init,            /* postconfiguration */
+  NULL,                                         /* create main configuration */
+  NULL,                                         /* init main configuration */
+  NULL,                                         /* create server configuration */
+  NULL,                                         /* merge server configuration */
+  ngx_http_bot_verifier_module_create_loc_conf, /* create location configuration */
+  ngx_http_bot_verifier_module_merge_loc_conf   /* merge location configuration */
 };
 
 ngx_module_t ngx_http_bot_verifier_module = {
   NGX_MODULE_V1,
-  &ngx_http_bot_verifier_module_ctx, /* module context */
-  NULL,                              /* module directives */
-  NGX_HTTP_MODULE,                   /* module type */
-  NULL,                              /* init master */
-  NULL,                              /* init module */
-  NULL,                              /* init process */
-  NULL,                              /* init thread */
-  NULL,                              /* exit thread */
-  NULL,                              /* exit process */
-  NULL,                              /* exit master */
+  &ngx_http_bot_verifier_module_ctx,     /* module context */
+  ngx_http_bot_verifier_module_commands, /* module directives */
+  NGX_HTTP_MODULE,                       /* module type */
+  NULL,                                  /* init master */
+  NULL,                                  /* init module */
+  NULL,                                  /* init process */
+  NULL,                                  /* init thread */
+  NULL,                                  /* exit thread */
+  NULL,                                  /* exit process */
+  NULL,                                  /* exit master */
   NGX_MODULE_V1_PADDING
 };
