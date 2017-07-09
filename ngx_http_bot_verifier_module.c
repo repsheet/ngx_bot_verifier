0d3a02a94214d6794b53db8362a7196c5f82140e
Aaron Bedra <aaron@aaronbedra.com>
2017-07-09 13:56:51 -0500

    Adding basic identification

--- ngx_http_bot_verifier_module.c
+++ ngx_http_bot_verifier_module.c
@@ -10,6 +10,46 @@ typedef struct {
 } ngx_http_bot_verifier_module_loc_conf_t;
 
 static ngx_int_t
+ngx_http_bot_verifier_module_identifies_as_known_bot(ngx_http_request_t *r)
+{
+  ngx_regex_t *re;
+  ngx_regex_compile_t rc;
+  u_char errstr[NGX_MAX_CONF_ERRSTR];
+
+  ngx_str_t user_agent = r->headers_in.user_agent->value;
+  if (user_agent.data == NULL) {
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "User Agent was not provided");
+    return NGX_DECLINED;
+  }
+
+  ngx_str_t pattern = ngx_string("Google");
+  ngx_memzero(&rc, sizeof(ngx_regex_compile_t));
+  rc.pattern = pattern;
+  rc.pool = r->pool;
+  rc.err.len = NGX_MAX_CONF_ERRSTR;
+  rc.err.data = errstr;
+
+  if (ngx_regex_compile(&rc) != NGX_OK) {
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Regex error: %V", &rc.err);
+    return NGX_ERROR;
+  }
+
+  re = rc.regex;
+
+  ngx_int_t n;
+  int captures[(1 + rc.captures) * 3];
+  n = ngx_regex_exec(re, &user_agent, captures, (1 + rc.captures) * 3);
+
+  if (n >= 0) {
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Google bot identified for %V", &user_agent);
+    return NGX_OK;
+  }
+
+  ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "User Agent %V not identified", &user_agent);
+  return NGX_DECLINED;
+}
+
+static ngx_int_t
 ngx_http_bot_verifier_module_handler(ngx_http_request_t *r)
 {
   if (r->main->internal) {
@@ -22,7 +62,12 @@ ngx_http_bot_verifier_module_handler(ngx_http_request_t *r)
     return NGX_DECLINED;
   }
 
-  ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Hello World");
+  ngx_int_t ret = ngx_http_bot_verifier_module_identifies_as_known_bot(r);
+  if (ret == NGX_OK) {
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Bot identity detected");
+  } else {
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Bot does not identify");
+  }
 
   return NGX_OK;
 }
